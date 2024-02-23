2d61e8ada9d4c319108a87827de5461ee338957e
yoanpiron84 <yoan.piron@alumni.univ-avignon.fr>
2024-02-23 15:02:36 +0100

    Ajout du traitement de l'API pour la récupération des titres / images / résumés et trailer YouTube des films

--- AdopteUnFilm/src/app/app.component.ts
+++ AdopteUnFilm/src/app/app.component.ts
@@ -1,13 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FilmsComponent } from './films/films.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FilmsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'AdopteUnFilm';
+
 }
