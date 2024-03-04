8e31583d5c1251dee483be87a00ddace300b0a27
abdelghanirechidi <ghanipro84@gmail.com>
2024-03-04 16:11:14 +0100

    commit test

--- js/server.js
+++ js/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const http = require('http');
 const path = require('path');
+const https = require("https");
+const cors = require('cors');
+const fs = require("fs");
 
 const app = express();
-const PORT = 3111;
+app.use(cors());
 
 // Définir le chemin vers le répertoire des fichiers statiques
-const staticPath = path.join(__dirname, '..', 'AdopteUnFilm', 'dist', 'adopte-un-film','browser');
+const staticPath = path.join(__dirname, '..', 'AdopteUnFilm', 'dist', 'adopte-un-film', 'browser');
 
 // Servir les fichiers statiques
 app.use(express.static(staticPath));
@@ -16,10 +18,13 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(staticPath, 'index.html'));
 });
 
-// Créer un serveur HTTPS sans certificat
-const server = http.createServer(app);
+// On utilise notre clé et certificat pour la connexion https
+const options = {
+    key: fs.readFileSync("key.pem"),
+    cert: fs.readFileSync("cert.pem"),
+};
 
-// Démarrer le serveur HTTPS
-server.listen(PORT, () => {
-    console.log('HTTPS server is running on port', PORT);
+// On se connecte à notre port d'écoute HTTPS
+const server = https.createServer(options, app).listen(3115, () => {
+    console.log("HTTPS => listening on 3115");
 });
